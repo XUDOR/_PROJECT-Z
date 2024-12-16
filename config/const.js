9502f22f72b1c4f827a8b08ba8d4c380a3df8906
Roderick Shoolbraid <rdxenv@gmail.com>
2024-12-16 15:19:48 -0800

    started

---
+++ config/const.js
@@ -0,0 +1,8 @@
+// const.js 
+module.exports = {
+  PROJECT_F_URL: 'http://localhost:3006/api/messages',    // For API messages
+  PROJECT_F_NOTIFICATIONS_URL: 'http://localhost:3006/api/notifications', // For general notifications
+  PROJECT_B_URL: 'http://localhost:3002',
+  PROJECT_A_URL: 'http://localhost:3001/api/receive-jobs',
+};
+
