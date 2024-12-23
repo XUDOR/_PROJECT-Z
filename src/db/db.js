fea56fcc703ab6310881b341aa8d626113d994b1
Roderick Shoolbraid <rdxenv@gmail.com>
2024-12-23 10:54:32 -0800

    changed SSL

--- src/db/db.js
+++ src/db/db.js
@@ -10,7 +10,7 @@ const pool = new Pool({
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
-    ssl: { rejectUnauthorized: false }, // Use proper certs in production
+    ssl: false
 });
 
 module.exports = pool;
